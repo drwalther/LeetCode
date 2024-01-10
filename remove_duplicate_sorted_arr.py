e2d2f1c48ff04e440f9599b2e07ef64aabceb101
Rakhaev Artem <a.u.rahaev@gmail.com>
2024-01-10 17:39:16 +0300

    add remove duplicate from sorted array

---
+++ remove_duplicate_sorted_arr.py
@@ -0,0 +1,19 @@
+# Удалить дубликаты из отсортированного массива
+
+
+class Solution:
+    def remove_duplicates(self, nums: list[int]) -> int:
+        j = 0
+
+        for i in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                j += 1
+                nums[j] = nums[i]
+
+        return j + 1
+
+
+if __name__ == "__main__":
+    s = Solution()
+    a = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    print(s.remove_duplicates(a))
